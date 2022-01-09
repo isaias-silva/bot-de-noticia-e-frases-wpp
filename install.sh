d5a502d090ef395710df7a5cd650125a182236ed
isaias-silva <zackblackmangold@gmail.com>
2022-01-09 14:54:53 -0300

    install

---
+++ install.sh
@@ -0,0 +1,6 @@
+apt-get update
+apt-get upgrade
+apt-get install nodejs
+npm install
+npm i -g puppeteer && puppeteer-extra && readline-sync && selenium-webdriver
+echo "[*] thankyou for use the bot!"
